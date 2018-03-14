e07cf1cfcb4174d467d2a94bc270ba3a0acdc66c
Elioth Monroy Martos <ianmendozajaimes@gmail.com>
2018-03-14 08:48:35 -0600

    lel

---
+++ corrimiento.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=corrimiento.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/corrimiento.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=corrimiento.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/corrimiento.x.tar
