6784211720420063cf34bec833293485ff3fff0c
Tonatihu <carlostonatihu@gmail.com>
2018-04-15 16:56:06 -0500

    Mas notas :poop:

--- Timer.X/interrupciones.s
+++ Timer.X/interrupciones.s
@@ -8,48 +8,49 @@
 	.global _NOTA_SOL
 	.global _NOTA_LA
 	.global _NOTA_SI
+
 	
 /**@brief LAS SIGUIENTES FUNCIONES SON LAS NOTAS A UTILIZAR
 ; */
 _NOTA_DO:
     CLR TMR1
-    SETM PR1
+    MOV #55, PR1
     MOV #0X8020, T1CON
     RETURN
     
 _NOTA_RE:
     CLR TMR1
-    SETM PR1
+    MOV #49, PR1
     MOV #0X8020, T1CON
     RETURN
 
 _NOTA_MI:
     CLR TMR1
-    SETM PR1
+    MOV #11, PR1
     MOV #0X8020, T1CON
     RETURN
 
 _NOTA_FA:
     CLR TMR1
-    SETM PR1
+    MOV #2639, PR1
     MOV #0X8020, T1CON
     RETURN
     
 _NOTA_SOL:
     CLR TMR1
-    SETM PR1
+    MOV #2351, PR1
     MOV #0X8020, T1CON
     RETURN
     
 _NOTA_LA:
     CLR TMR1
-    SETM PR1
+    MOV #8, PR1
     MOV #0X8020, T1CON
     RETURN
     
 _NOTA_SI:
     CLR TMR1
-    SETM PR1
+    MOV #1866, PR1
     MOV #0X8020, T1CON
     RETURN
     
@@ -59,26 +60,8 @@ _NOTA_SI:
 ; */
 __INT0Interrupt:
     PUSH W0
-    INC _uni
-    CP _uni, #10
-    BRA NZ, FIN_INTERRUPCION ; SI NO ES IGUAL A 10 SALTA
-    CLR _uni
-    INC _dece
-    CP _dece, #10
-    BRA NZ, FIN_INTERRUPCION
-    CLR _dece
-    INC _cen
-    CP _cen, #10
-    BRA NZ, FIN_INTERRUPCION
-    CLR _cen
-    INC _umi
-    CP _umi, #10
-    BRA NZ, FIN_INTERRUPCION
-    CLR _umi
-    
-FIN_INTERRUPCION:
+    BGT LATD, #RD3
     BCLR    IFS0,   #INT0IF
     POP	    W0
     RETFIE
-    
     
