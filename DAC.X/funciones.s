ece946a277431eb3e975ad97c565a084f2c0640a
Tonatihu <carlostonatihu@gmail.com>
2018-06-01 23:27:17 -0500

    Falta completar las funcione

--- DAC.X/funciones.s
+++ DAC.X/funciones.s
@@ -1,17 +1,25 @@
         .include "p30F4013.inc"
 
-        .global _CONV_CODIGO
+        .global _RD_WR_SPI
 	
 /**@brief 
 ; * @PARAM:
 ; */
-_CONV_CODIGO:
+_RD_WR_SPI:
     MOV	SPI1BUF, W0
     BCLR IFS0, #SPI1IF
 CICLO:
     BTSS IFS0,	#SPI1IF
     GOTO CICLO
     MOV	W0,	SPI1BUF
+    
+    RETURN
+    
+_WR_DAC:
+    AND W0, #0X0FFF
+    BSET W0, #12
+    CALL RD_WR_SPI
+    NOP
     
     RETURN
     
---
+++ DAC.X/interrupciones.s
@@ -0,0 +1,17 @@
+        .include "p30F4013.inc"
+
+	.global __U2RXInterrupt
+	
+		
+__U2RXInterrupt:
+    PUSH W0
+    
+    MOV U2RXREG, W0
+    MOV W0,	U1TXREG
+    NOP
+
+    BCLR    IFS1,   #U2RXIF
+    POP	    W0
+    RETFIE
+    
+    
